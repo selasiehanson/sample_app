070f839c7a1155e421f713428b71587cfb2a7eab
Selsie Hanson <code735@gmail.com>
2012-11-05 15:34:25 +0000

    added a saticpages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '58c219ecdcdf5f9f66cfa5af05d741076d9e683f9fc651ba57cd6d8b647a7d75cee6a9bdd47eb01a116c18da98d8fc798d417e5e10980fa432613ecb4f56257f'
